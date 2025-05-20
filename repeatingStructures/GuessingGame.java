8539deedeed40dc1c843d7c5f54937a4afb9b2f4
vsilva.wayon <viniciusroqf@gmail.com>
2025-05-19 21:47:42 -0300

    feat: jogo da advinhacao

---
+++ repeatingStructures/GuessingGame.java
@@ -0,0 +1,50 @@
+package repeatingStructures;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+    public static void main(String[] args) {
+        
+        /*
+         * Exercício 2: Jogo da adivinhação
+            Criar um número aleatório;
+            Pedir uum para o usuário;
+            Salvar as tentativas que foram usadas;
+            Enquanto o usuário não acertar, exibir se o número é maior ou menor que o número criado;
+            Colocar um total de 40 tentativas;
+            Exibir a mnsagem de sucsso ao acertar, incluindo a quantidade de tentativas.
+         */
+
+        Random random = new Random();
+
+        int randomNumber = random.nextInt(100);
+
+        System.out.println("Esse é o jogo da advinhação, digite um número inteiro e tente acertar: ");
+
+        guessingGame(randomNumber);
+    }
+
+    /**
+     * Jogo da advinhação
+     * @param randomNumber
+     */
+    public static void guessingGame(int randomNumber) {
+        Scanner scanner = new Scanner(System.in);
+
+        externo: 
+        for(int i = 0; i <= 40; i++){
+            int number = scanner.nextInt();
+
+            if (number == randomNumber) {
+                System.out.println("Você acertou!");
+                break externo;
+            } else if ( number < randomNumber) {
+                System.out.println("Você errou, o número é MAIOR que o digitado.");
+            } else if (number > randomNumber) {
+                System.out.println("Você errou, o número é MENOR que o digitado.");
+            } 
+            continue externo;
+        }   
+    }
+}
