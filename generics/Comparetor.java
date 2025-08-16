034f972aa6e004a667fb32b8b2193a317498790a
vsilva.wayon <viniciusroqf@gmail.com>
2025-08-16 11:11:39 -0300

    feat: bounded types

---
+++ generics/Comparetor.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class Comparetor<T extends Number>  {
+
+    public T obterMaior(T numero1, T numero2) {
+
+        // >
+        if(numero1.doubleValue() > numero2.doubleValue()) {
+            return numero1;
+        } else {
+            return numero2;
+        }
+
+    }
+}
--- generics/Generics.java
+++ generics/Generics.java
@@ -17,6 +17,18 @@ public class Generics {
         //2- Criando métodos genérics
         System.out.println("Valor maior entre 25 e 40 é: " + getGreaterValue(25, 40));
 
+        // 3 - bounded types
+        Comparetor<Integer> comparadorInteiros = new Comparetor<>();
+
+        System.out.println("Maior numero entre 20 e 10: " + comparadorInteiros.obterMaior(20, 10));
+
+        Comparetor<Double> comparadorInteiros2 = new Comparetor<>();
+
+        System.out.println("Maior numero entre 1.95 e 4.55: " + comparadorInteiros2.obterMaior(1.95, 4.55));
+    
+        // Erro de compilação
+        // Comparador<String> comparadorStrings = new Comparador<>();
+
     }
 
     public static<T extends Comparable<T>> T getGreaterValue( T value1, T value2) {
