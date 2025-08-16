fcb9407c8249039a444d0ea5b7da1294b139da13
vsilva.wayon <viniciusroqf@gmail.com>
2025-08-16 11:34:15 -0300

    feat: tipos coringas(wildcards)

--- generics/Generics.java
+++ generics/Generics.java
@@ -29,9 +29,65 @@ public class Generics {
         // Erro de compilação
         // Comparador<String> comparadorStrings = new Comparador<>();
 
+        // 4 - wildcard
+        List<Integer> numeros2 = List.of(1, 2, 3);
+        List<String> palavras2 = List.of("teste", "Java", "olá");
+
+        // aceita qualquer tipo de lista (?)
+        imprimirLista(numeros2);
+
+        imprimirLista(palavras2);
+
+        // aceita listas numericas (? extends Number)
+        System.out.println(somarNumeros(numeros2));
+
+        // somarNumeros(palavras2);
+
+        // aceita apenas tipos numericos especificos
+        // Number > Integer (5,4,3)
+        List<Integer> numeros3 = new ArrayList<>();
+
+        // adicionarNumeros aceita apenas Lista de Inteiros
+        adicionarNumeros(numeros3);
+
+        for(Integer numero : numeros3) {
+            System.out.println(numero);
+        }
     }
 
     public static<T extends Comparable<T>> T getGreaterValue( T value1, T value2) {
         return (value1.compareTo(value2) > 0) ? value1 : value2;
     }
+
+    public static <K> void imprimirArray(K[] array) {
+        for(K elemento : array) {
+            System.out.print(elemento + ", ");
+        }
+    }
+
+    public static void imprimirLista(List<?> lista) {
+        for(Object elemento : lista) {
+            System.out.println(elemento);
+        }
+    }
+
+    public static double somarNumeros(List<? extends Number> lista) {
+        double soma = 0;
+
+        for(Number numero : lista) {
+            soma += numero.doubleValue();
+        }
+
+        return soma;
+
+    }
+
+    public static void adicionarNumeros(List<? super Integer> lista) {
+
+        for(int i = 1; i <= 5; i++) {
+            lista.add(i);
+        }
+
+    }
+
 }
